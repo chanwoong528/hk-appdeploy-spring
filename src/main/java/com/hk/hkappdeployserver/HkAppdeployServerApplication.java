daebe3970c1ef46db86a10ea4ece77f7082e3e4e
문찬웅 <mooncw@hankookilbo.com>
2023-12-21 17:45:16 +0900

    first commit

---
+++ src/main/java/com/hk/hkappdeployserver/HkAppdeployServerApplication.java
@@ -0,0 +1,13 @@
+package com.hk.hkappdeployserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkAppdeployServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HkAppdeployServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hk/hkappdeployserver/HkAppdeployServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hk.hkappdeployserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkAppdeployServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
